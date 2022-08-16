1bd57f01629278993ac4a7d40f3f7c525f52378f
Gypsy Rogers <github@gypsy.org>
2022-08-16 01:45:21 -0700

    Everything seems to be working now

--- mounts3.sh
+++ mounts3.sh
@@ -11,8 +11,7 @@ mountbase="$HOME/s3"
 
 
 #unmount everything to start fresh
-oldIFS=$IF
-IFS=$'\n'
+IFS=$'\n' #to split on newline
 for line in $(mount | grep "s3fs");do
 	#example line:  s3fs on /home/gypsy/s3/imaginationguild-devshare type fuse.s3fs (rw,nosuid,nodev,relatime,user_id=1000,group_id=1000)
 	mountpoint="$(cut -d' ' -f3 <<<\"$line\" )"
@@ -21,25 +20,20 @@ for line in $(mount | grep "s3fs");do
 		`umount $mountpoint`
 	fi
 done
-IFS=$oldIFS
-
-
-txt="echo 'blah'"
-echo $txt
-exit
+unset IFS #reset to default split
 
 
 
 #delete unused dirs
-echo "delete time"
+#echo "delete time"
 `rm -r  $mountbase`
 #make sure we have the s3 directory to mount to
-mkdir -p ~/s3
+mkdir -p $mountbase
 
 
 #itterate through the list of profiles
 for profile in $profiles; do  #itterate through profiles
-    echo "mounting buckets from profile: $profile:"
+    echo "Mounting buckets from $profile:"
     #this is hacky but it works since aws cli doesn't have a way to query the secret key that I could find
     access_key=`grep -A 5 -m 1 "[$profile]" ~/.aws/credentials | grep -m 1 aws_access_key_id | awk '{split($0,a,"\s*=\s*"); print a[2]}'`
     secret_key=`grep -A 5 -m 1 "[$profile]" ~/.aws/credentials | grep -m 1 aws_secret_access_key | awk '{split($0,a,"\s*=\s*"); print a[2]}'`
@@ -51,9 +45,25 @@ for profile in $profiles; do  #itterate through profiles
     buckets=`aws --profile $profile s3api list-buckets --query "Buckets[].Name" | sed ':a;N;$!ba;s/\n/ /g' | tr -s ' ' | sed 's/^...\(.*\)...$/\1/' | sed 's/", "/ /g'`
 
     for bucket in $buckets; do
-	#TODO: if it has a dot add this flag: -o use_path_request_style
-        echo "/usr/bin/s3fs $bucket $mountpoint/$bucket -o passwd_file=~/.aws/passwd-s3fs-$profile"
-        /usr/bin/s3fs $bucket $mountpoint/$bucket -o passwd_file=~/.aws/passwd-s3fs-$profile
+	#if it has a dot add this flag: -o use_path_request_style
+	option=""
+	if [[ $bucket =~ "." ]]; then
+		option=" -o use_path_request_style "
+	fi
+	mkdir -p $mountbase/$bucket
+	cmd="/usr/bin/s3fs $option $bucket $mountbase/$bucket -o passwd_file=~/.aws/passwd-s3fs-$profile"
+	#echo "$cmd"
+	eval "$cmd" #using eval to create the command in only one place but still show it on the screen
+	mountgrep=`mount | grep "s3fs" | grep "$bucket"`
+	#echo "mountgrep: $mountgrep"
+	if [ -z "$mountgrep" ]; then
+		echo "   There seems to have been a problem mounting $bucket to $mountbase/$bucket to troubleshoot run:"
+		echo "          $cmd -f"
+	else
+		echo "   Mounted: $bucket"	
+	fi
+
+
     done
 
 
