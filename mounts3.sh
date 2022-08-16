206ad0e61a635c757579eacdad0274cf4c60377c
Gypsy Rogers <github@gypsy.org>
2022-08-15 23:20:50 -0700

    just saving my work

--- mounts3.sh
+++ mounts3.sh
@@ -7,13 +7,42 @@
 #get a list of profiles
 profiles=$(aws configure list-profiles)
 
+mountbase="$HOME/s3"
+
+
+#unmount everything to start fresh
+oldIFS=$IF
+IFS=$'\n'
+for line in $(mount | grep "s3fs");do
+	#example line:  s3fs on /home/gypsy/s3/imaginationguild-devshare type fuse.s3fs (rw,nosuid,nodev,relatime,user_id=1000,group_id=1000)
+	mountpoint="$(cut -d' ' -f3 <<<\"$line\" )"
+	if [[ $line =~ "$mountbase"* ]]; then
+		echo "unmounting $mountpoint"
+		`umount $mountpoint`
+	fi
+done
+IFS=$oldIFS
+
+
+txt="echo 'blah'"
+echo $txt
+exit
+
+
+
+#delete unused dirs
+echo "delete time"
+`rm -r  $mountbase`
 #make sure we have the s3 directory to mount to
 mkdir -p ~/s3
 
+
+#itterate through the list of profiles
 for profile in $profiles; do  #itterate through profiles
+    echo "mounting buckets from profile: $profile:"
     #this is hacky but it works since aws cli doesn't have a way to query the secret key that I could find
-    access_key=`grep -A 5 -m 1 "[$profile]" ~/.aws/credentials | grep -m 1 aws_access_key_id | awk '{split($0,a," = "); print a[2]}'`
-    secret_key=`grep -A 5 -m 1 "[$profile]" ~/.aws/credentials | grep -m 1 aws_secret_access_key | awk '{split($0,a," = "); print a[2]}'`
+    access_key=`grep -A 5 -m 1 "[$profile]" ~/.aws/credentials | grep -m 1 aws_access_key_id | awk '{split($0,a,"\s*=\s*"); print a[2]}'`
+    secret_key=`grep -A 5 -m 1 "[$profile]" ~/.aws/credentials | grep -m 1 aws_secret_access_key | awk '{split($0,a,"\s*=\s*"); print a[2]}'`
     #create the password file for mounting
     echo "$access_key:$secret_key" > ~/.aws/passwd-s3fs-$profile
     /usr/bin/chmod 600 ~/.aws/passwd-s3fs-$profile
@@ -22,8 +51,9 @@ for profile in $profiles; do  #itterate through profiles
     buckets=`aws --profile $profile s3api list-buckets --query "Buckets[].Name" | sed ':a;N;$!ba;s/\n/ /g' | tr -s ' ' | sed 's/^...\(.*\)...$/\1/' | sed 's/", "/ /g'`
 
     for bucket in $buckets; do
-        mkdir -p ~/s3/$bucket
-        /usr/bin/s3fs $bucket ~/s3/$bucket -o passwd_file=~/.aws/passwd-s3fs-$profile
+	#TODO: if it has a dot add this flag: -o use_path_request_style
+        echo "/usr/bin/s3fs $bucket $mountpoint/$bucket -o passwd_file=~/.aws/passwd-s3fs-$profile"
+        /usr/bin/s3fs $bucket $mountpoint/$bucket -o passwd_file=~/.aws/passwd-s3fs-$profile
     done
 
 
