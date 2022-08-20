9161b096010bdd57f5c32266e7f22dd92e54fb6b
Gypsy Rogers <github@gypsy.org>
2022-08-20 11:04:03 -0700

    This seems to be working across multiple regions now

--- mounts3.sh
+++ mounts3.sh
@@ -38,8 +38,9 @@ mkdir -p $mountbase
 for profile in $profiles; do  #itterate through profiles
     echo "Mounting buckets from $profile:"
     #this is hacky but it works since aws cli doesn't have a way to query the secret key that I could find
-    access_key=`grep -A 5 -m 1 "[$profile]" ~/.aws/credentials | grep -m 1 aws_access_key_id | awk '{split($0,a,"\s*=\s*"); print a[2]}' | tr -d ' '`
-    secret_key=`grep -A 5 -m 1 "[$profile]" ~/.aws/credentials | grep -m 1 aws_secret_access_key | awk '{split($0,a,"\s*=\s*"); print a[2]}' | tr -d ' '`
+    #TODO: should probably grep out comments and empty lines to make sure that the -A5 has a better chance of working but this is working for me for now
+    access_key=`grep -A 5 -m 1 "\[$profile\]" ~/.aws/credentials | grep -m 1 aws_access_key_id | awk '{split($0,a,"\s*=\s*"); print a[2]}' | tr -d ' '`
+    secret_key=`grep -A 5 -m 1 "\[$profile\]" ~/.aws/credentials | grep -m 1 aws_secret_access_key | awk '{split($0,a,"\s*=\s*"); print a[2]}' | tr -d ' '`
     #create the password file for mounting
     echo "$access_key:$secret_key" > ~/.aws/passwd-s3fs-$profile
     /usr/bin/chmod 600 ~/.aws/passwd-s3fs-$profile
@@ -54,6 +55,15 @@ for profile in $profiles; do  #itterate through profiles
 		option=" -o use_path_request_style "
 	fi
 	mkdir -p $mountbase/$bucket
+
+	#manage endpoint to get around defaults
+	locationinfo="$(aws s3api get-bucket-location --profile=$profile --bucket $bucket)"
+	reg='\"LocationConstraint\": \"(.*?)\"'
+	[[ "$locationinfo" =~ $reg ]] 
+	location="${BASH_REMATCH[1]}"
+	option="$option  -o url=https://s3-$location.amazonaws.com "
+
+
 	cmd="/usr/bin/s3fs -o check_cache_dir_exist  $option $bucket $mountbase/$bucket -o passwd_file=~/.aws/passwd-s3fs-$profile "
 	#echo "$cmd"
 	eval "$cmd" #using eval to create the command in only one place but still show it on the screen
@@ -65,7 +75,7 @@ for profile in $profiles; do  #itterate through profiles
 		mountgrep=`mount | grep "s3fs" | grep "$bucket"`
 		if [ -z "$mountgrep" ]; then
 			echo "  There seems to have been a problem mounting $bucket to $mountbase/$bucket to troubleshoot run:"
-			echo "    $cmd -f"
+			echo "    $cmd -o dbglevel=info -f"
 			processing=false
 		else
 			echo "Looks like we are still trying to mount $bucket, waiting 5 seconds"
